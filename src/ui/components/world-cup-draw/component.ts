c0ab42529057cfdc364d244b70921526cc5eb96d
Jorge Bejar <jorge@wyeworks.com>
2017-10-16 18:22:00 -0300

    Add a component for world cup groups

--- src/ui/components/world-cup-draw/component.ts
+++ src/ui/components/world-cup-draw/component.ts
@@ -1,5 +1,7 @@
 import Component from '@glimmer/component';
 
 export default class WorldCupDraw extends Component {
-
+  get groupA() {
+    return { teams: ['Argentina', 'Italia', 'Panama', 'Corea del Sur'] };
+  }
 }
