ea8c098c6295577bc9de64b7918f6144d316023e
Jorge Bejar <jorge@wyeworks.com>
2017-10-18 15:12:00 -0300

    Add teams to pots

--- src/utils/pots.ts
+++ src/utils/pots.ts
@@ -8,6 +8,36 @@ const POTS = [
     { name: 'Bélgica', confederation: 'UEFA' },
     { name: 'Argentina', confederation: 'CONMEBOL' },
     { name: 'Brasil', confederation: 'CONMEBOL' },
+  ],
+  [
+    { name: 'España', confederation: 'UEFA' },
+    { name: 'Perú', confederation: 'CONMEBOL' },
+    { name: 'Suiza', confederation: 'UEFA' },
+    { name: 'Inglaterra', confederation: 'UEFA' },
+    { name: 'Colombia', confederation: 'CONMEBOL' },
+    { name: 'Italia', confederation: 'UEFA' },
+    { name: 'México', confederation: 'CONCACAF' },
+    { name: 'Uruguay', confederation: 'CONMEBOL' },
+  ],
+  [
+    { name: 'Croacia', confederation: 'UEFA' },
+    { name: 'Dinamarca', confederation: 'UEFA' },
+    { name: 'Islandia', confederation: 'UEFA' },
+    { name: 'Costa Rica', confederation: 'CONCACAF' },
+    { name: 'Tunez', confederation: 'CAF' },
+    { name: 'Egipto', confederation: 'CAF' },
+    { name: 'Senegal', confederation: 'CAF' },
+    { name: 'Irán', confederation: 'AFC' },
+  ],
+  [
+    { name: 'Serbia', confederation: 'UEFA' },
+    { name: 'Nigeria', confederation: 'CAF' },
+    { name: 'Japón', confederation: 'AFC' },
+    { name: 'Panamá', confederation: 'CONCACAF' },
+    { name: 'Costa de Marfil', confederation: 'CAF' },
+    { name: 'Corea del Sur', confederation: 'AFC' },
+    { name: 'Arabia Saudita', confederation: 'AFC' },
+    { name: 'Honduras', confederation: 'CONCACAF' },
   ]
 ];
 
