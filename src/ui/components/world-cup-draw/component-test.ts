199aafd53e0e137b8333164029735492e9319481
Jorge Bejar <jorge@wyeworks.com>
2017-10-29 08:57:19 -0300

    Rewrite test with jQuery

--- src/ui/components/world-cup-draw/component-test.ts
+++ src/ui/components/world-cup-draw/component-test.ts
@@ -1,5 +1,6 @@
 import { setupRenderingTest } from '@glimmer/test-helpers';
 import hbs from '@glimmer/inline-precompile';
+import 'jquery';
 
 const { module, test } = QUnit;
 
@@ -8,37 +9,37 @@ module('Component: world-cup-draw', function(hooks) {
 
   test('it renders a title', async function(assert) {
     await this.render(hbs`<world-cup-draw />`);
-    assert.equal(this.containerElement.querySelector('h1').textContent, 'Russia 2018 World Cup Draw');
+    assert.equal($(this.containerElement).find('h1').text(), 'Russia 2018 World Cup Draw');
   });
 
   test('it renders the pots', async function(assert) {
     await this.render(hbs`<world-cup-draw />`);
-    const potElements:[HTMLElement] = this.containerElement.querySelectorAll('.pot-container .pot-item');
-
+    const potElements = $(this.containerElement).find('.pot-container .pot-item');
+    
     assert.equal(potElements.length, 4);
     for (var i = 0; i < 4; i++) {
-      const titleElement = potElements[i].querySelector('.pot-title');
-      assert.equal(titleElement.textContent, `Pot ${i+1}`);
+      const titleElement = $(potElements[i]).find('.pot-title');
+      assert.equal(titleElement.text(), `Pot ${i+1}`);
     }
   });
 
   test('it renders the groups', async function(assert) {
     await this.render(hbs`<world-cup-draw />`);
-    const potElements:[HTMLElement] = this.containerElement.querySelectorAll('.group-container .group-item');
+    const potElements = $(this.containerElement).find('.group-container .group-item');
 
     assert.equal(potElements.length, 8);
 
     const charForInt = (i) => String.fromCharCode('A'.charCodeAt(0) + i);
     for (var i = 0; i < 8; i++) {
-      const titleElement = potElements[i].querySelector('.group-title');
-      assert.equal(titleElement.textContent, `Group ${charForInt(i)}`);
+      const titleElement = $(potElements[i]).find('.group-title');
+      assert.equal(titleElement.text(), `Group ${charForInt(i)}`);
     }
   });
 
   test('it renders a reset button', async function(assert) {
     await this.render(hbs`<world-cup-draw />`);
-    const buttonElement = this.containerElement.querySelector('.main-container > .draw-button-container button');
+    const buttonElement = $(this.containerElement).find('.main-container > .draw-button-container button');
 
-    assert.equal(buttonElement.textContent, 'Reset');
+    assert.equal(buttonElement.text(), 'Reset');
   });
 });
