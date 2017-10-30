6dbf1be787edeec21f76f23b5121a48bdc70a15f
Jorge Bejar <jorge@wyeworks.com>
2017-10-30 15:43:34 -0300

    Draw a team from the list of teams in the pot

--- src/ui/components/DrawPot/component.ts
+++ src/ui/components/DrawPot/component.ts
@@ -2,6 +2,13 @@ import Component from '@glimmer/component';
 
 export default class DrawPot extends Component {
   draw() {
-    alert("Draw a team!");
+    alert(`The selected team is: ${this._drawTeam()}`);
+  }
+
+  _drawTeam(): string {
+    const teams: string[] = this.args.teams;
+    const randomIndex = Math.floor(Math.random() * teams.length);
+
+    return teams[randomIndex];
   }
 };
