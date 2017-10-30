fe939a4435a534edec0d01a0f2b7d1ecb5850f65
Jorge Bejar <jorge@wyeworks.com>
2017-10-30 17:43:05 -0300

    Pass teams into Groups components

--- src/ui/components/WorldCupDraw/component.ts
+++ src/ui/components/WorldCupDraw/component.ts
@@ -2,4 +2,5 @@ import Component from '@glimmer/component';
 
 export default class WorldCupDraw extends Component {
   teams: string[] = ['Alemania', 'Polonia', 'Portugal', 'Francia', 'Bélgica', 'Argentina', 'Brasil']
+  group: string[] = Array(4)
 }
