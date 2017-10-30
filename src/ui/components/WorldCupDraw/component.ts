2e932566520fc07a5cb17f7cc007765989e0adfa
Jorge Bejar <jorge@wyeworks.com>
2017-10-29 23:57:24 -0300

    Show teams in pot 1

--- src/ui/components/WorldCupDraw/component.ts
+++ src/ui/components/WorldCupDraw/component.ts
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
 
 export default class WorldCupDraw extends Component {
-
+  teams: string[] = ['Alemania', 'Polonia', 'Portugal', 'Francia', 'Bélgica', 'Argentina', 'Brasil']
 }
