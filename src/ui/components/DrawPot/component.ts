0bebe68bde94117d952c47c8fc353a350df4547e
Jorge Bejar <jorge@wyeworks.com>
2017-10-30 18:53:54 -0300

    Draw a team from pot 1 and place it in a group

--- src/ui/components/DrawPot/component.ts
+++ src/ui/components/DrawPot/component.ts
@@ -1,21 +1,10 @@
 import Component, { tracked } from '@glimmer/component';
 
 export default class DrawPot extends Component {
-  @tracked public selectedTeam: string;
+  @tracked public drawDone: boolean = false;
 
   public draw(): void {
-    this.selectedTeam = this.drawTeam();
-  }
-
-  @tracked('selectedTeam')
-  get drawDone(): boolean {
-    return !!this.selectedTeam;
-  }
-
-  private drawTeam(): string {
-    const teams: string[] = this.args.teams;
-    const randomIndex = Math.floor(Math.random() * teams.length);
-
-    return teams[randomIndex];
+    this.drawDone = true;
+    this.args.drawAction();
   }
 }
--- src/ui/components/WorldCupDraw/component.ts
+++ src/ui/components/WorldCupDraw/component.ts
@@ -1,6 +1,13 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
 
 export default class WorldCupDraw extends Component {
   public teams: string[] = ['Alemania', 'Polonia', 'Portugal', 'Francia', 'Bélgica', 'Argentina', 'Brasil'];
-  public group: string[] = Array(4);
+  @tracked public group: string[] = Array(4);
+
+  public drawTeamFromPot(): void {
+    const randomIndex = Math.floor(Math.random() * this.teams.length);
+    const selectedTeam: string = this.teams[randomIndex];
+
+    this.group = [selectedTeam, ...this.group.slice(1)];
+  }
 }
