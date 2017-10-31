0a1a383e35e4e8bd8dabab892947e06d447b7752
Jorge Bejar <jorge@wyeworks.com>
2017-10-31 15:34:18 -0300

    Add a boolean property to teams to track if they were already drawn

--- src/ui/components/WorldCupDraw/component.ts
+++ src/ui/components/WorldCupDraw/component.ts
@@ -1,13 +1,29 @@
 import Component, { tracked } from '@glimmer/component';
 
+interface ITeam {
+  name: string;
+  drawn?: boolean;
+}
+
 export default class WorldCupDraw extends Component {
-  public teams: string[] = ['Alemania', 'Polonia', 'Portugal', 'Francia', 'Bélgica', 'Argentina', 'Brasil'];
-  @tracked public group: string[] = Array(4);
+  public teams: ITeam[] = [
+    { name: 'Alemania' },
+    { name: 'Polonia' },
+    { name: 'Portugal' },
+    { name: 'Francia' },
+    { name: 'Bélgica' },
+    { name: 'Argentina' },
+    { name: 'Brasil' }
+  ];
+
+  @tracked public group: ITeam[] = Array(4);
 
   public drawTeamFromPot(): void {
-    const randomIndex = Math.floor(Math.random() * this.teams.length);
-    const selectedTeam: string = this.teams[randomIndex];
+    const availableTeams: ITeam[] = this.teams.filter((t) => !t.drawn);
+    const randomIndex = Math.floor(Math.random() * availableTeams.length);
+    const selectedTeam: ITeam = availableTeams[randomIndex];
 
+    selectedTeam.drawn = true;
     this.group = [selectedTeam, ...this.group.slice(1)];
   }
 }
