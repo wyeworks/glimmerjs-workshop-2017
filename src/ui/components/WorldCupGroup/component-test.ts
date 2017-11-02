35772f40e2ebe895c99093a54f31847c14d8dad6
Jorge Bejar <jorge@wyeworks.com>
2017-11-02 16:48:02 -0300

    Show group letter in WorldCupGroup components

--- src/ui/components/WorldCupGroup/component-test.ts
+++ src/ui/components/WorldCupGroup/component-test.ts
@@ -6,8 +6,19 @@ const { module, test } = QUnit;
 module('Component: WorldCupGroup', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    await this.render(hbs`<WorldCupGroup />`);
-    assert.ok(this.containerElement.querySelector('div'));
+  test('it renders the group A', async function(assert) {
+    await this.render(hbs`<WorldCupGroup @groupIndex={{0}} />`);
+
+    const titleElement = this.containerElement.querySelector('.group-title');
+
+    assert.equal(titleElement.textContent, 'Group A');
+  });
+
+  test('it renders the group B', async function(assert) {
+    await this.render(hbs`<WorldCupGroup @groupIndex={{1}} />`);
+
+    const titleElement = this.containerElement.querySelector('.group-title');
+
+    assert.equal(titleElement.textContent, 'Group B');
   });
 });
